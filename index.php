5c7c6d309bec6c3d633cf1b7d74f956ba337505c
ghostff <frankchris@hotmail.com>
2016-04-10 19:01:15 -0500

    paypal donation processor (80%)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once('funcs.php');
 $pay = payment('donate', 'email', 'string');
-$pay->info('USD', 50.30, 'chrys', 'ugwu', 'frankchris76@gmail.com', 'address2', null, 'city', 'tx', '5454', 'us', 'phone', null, null);
+$pay->info('USD', 'amt', 'fname', 'lname', 'email', 'addr1', (null|addr2), 'city', 'tx', '5454', 'country', 'phone', null, null);
 $pay->save(false, false, 'return');
 if(!$pay->error)
 	var_dump($pay->success);
